6cdb1d704221ddea29b0fd6f53cfee2851486319
nsantol <50126541+nsantol@users.noreply.github.com>
2019-04-29 23:09:50 -0400

    Delete MAKE PROJECT.py

--- MAKE PROJECT.py
+++
@@ -1,8 +0,0 @@
-for d in json_price_data:
-            price.append({'name': d['name'], 'price': float(d['amazon_price']), 'url': d['amazon_url']})
-            price.append({'name': d['name'], 'price': float(d['walmart_price']), 'url': d['walmart_url']})
-            price.append({'name': d['name'], 'price': float(d['ebay_price']), 'url': d['ebay_url']})
-            minPricedItem = min(price, key=lambda x: x['price'])
-            print(minPricedItem)
-            print('=================')
-            price = []
