2f90a060c62821b62e0bae11d9856d6d1f9f8159
danieladiaz04 <49916097+danieladiaz04@users.noreply.github.com>
2019-04-30 18:25:05 -0400

    Update sendemail.py

--- sendemail.py
+++ sendemail.py
@@ -1,5 +1,8 @@
 import smtplib
-
+import sched, time
+import pandas as pd
+import xlrd
+import datetime
 import config
 
 def send_email(subject, msg):
@@ -27,3 +30,30 @@ while True:
     schedule.run_pending()
     time.sleep(50)
 
+import config
+import sched, time
+import pandas as pd
+import xlrd
+import datetime
+
+
+localtime=time.asctime(time.localtime(time.time()))
+print("the time is " + localtime)
+
+#if localtime.startswith("Tue"):
+    #send_email(subject, msg)
+
+#localtime= localtime+604800
+
+#df = pd.read_excel ('tasks.xlsx')
+#print(df)
+
+file_location= "/Users/Daniela/PycharmProjects/untitled/tasks.xlsx"
+workbook= xlrd.open_workbook(file_location)
+sheet=workbook.sheet_by_index(0)
+due=sheet.cell_value(1,1)
+print(due)
+
+now = datetime.datetime.now()
+print ("Current date and time using str method of datetime object:")
+print (now.strftime("%Y-%m-%d %H:%M"))
