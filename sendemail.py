f6c5d57c3a970537d1774b73a33d20110d376ada
nsantol <50126541+nsantol@users.noreply.github.com>
2019-05-01 19:04:10 -0400

    Update sendemail.py

--- sendemail.py
+++ sendemail.py
@@ -1,9 +1,9 @@
 import smtplib #module for sending email
-import sched, time   
+import sched, time   #Sched module to schedule the program to run every certani time
 import pandas as pd
 import xlrd
 import datetime
-import config
+import config #module we created with login information. 
 
 def send_email(subject, msg):
     try:
@@ -22,6 +22,12 @@ subject = " Assignments Due"
 msg = "Hello"
 
 schedule.every().monday.at("08:00").do(send_email, subject, msg) #You can put any day and time, and module will work.
+schedule.every().tuesday.at("08:00").do(send_email, subject, msg) #We want the program to send an email every day in the morning, so we added every day.
+schedule.every().wednesday.at("08:00").do(send_email, subject, msg)
+schedule.every().thursday.at("08:00").do(send_email, subject, msg)
+schedule.every().friday.at("08:00").do(send_email, subject, msg)
+schedule.every().saturday.at("08:00").do(send_email, subject, msg)
+schedule.every().sunday.at("08:00").do(send_email, subject, msg)
 
 
 while True:  #Program need to be constantly running. But need to add sleep because computer can crash.
