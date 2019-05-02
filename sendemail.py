549dc04c75926f929507e76c1663f4679a2ca208
danieladiaz04 <49916097+danieladiaz04@users.noreply.github.com>
2019-05-01 21:01:33 -0400

    Update sendemail.py

--- sendemail.py
+++ sendemail.py
@@ -5,22 +5,6 @@ import datetime
 import config #module we created with login information. 
 import math
 
-def send_email(subject, msg):
-    try:
-        server = smtplib.SMTP('smtp.gmail.com:587') #If you want to use a different email, need to find the code for that one.
-        server.ehlo()
-        server.starttls()
-        server.login(config.EMAIL_ADDRESS, config.PASSWORD)  #config is from module we created. You can add any sender email, but need to allow less secure apps.
-        message = "Subject: {}\n\n{}".format(subject, msg)  #Use this configuration so the Subject appears on the "Subject" line.
-        server.sendmail(config.EMAIL_ADDRESS, config.RECEIVER_ADDRESS, message) #need to add RECEIVER_EMAIL to config module.
-        server.quit()
-        print("Success Email sent!")
-    except:
-        print("Email failed to send.")
-
-subject = " Assignments Due"
-msg = "Hello"
-
 schedule.every().monday.at("08:00").do(send_email, subject, msg) #You can put any day and time, and module will work.
 schedule.every().tuesday.at("08:00").do(send_email, subject, msg) #We want the program to send an email every day in the morning, so we added every day.
 schedule.every().wednesday.at("08:00").do(send_email, subject, msg)
@@ -59,24 +43,21 @@ def send_email(subject, msg):
         print("Email failed to send.")
 
 subject = "Things to do today"
-msg="hello"
+msg=""
 
 def excel_date(date1):
     temp = datetime.datetime(1899, 12, 30)
     delta = date1 - temp
     return(math.floor((delta.days) + (float(delta.seconds) / 86400)))
 
-file_location= "/Users/Daniela/PycharmProjects/untitled/tasks.xlsx"
-workbook= xlrd.open_workbook(file_location)
+workbook= xlrd.open_workbook("tasks.xlsx")
 sheet=workbook.sheet_by_index(0)
 
 now = datetime.datetime.now()
-now_date=(now.strftime("%Y-%m-%d %H:%m"))
-today = datetime.date.today()
 
 for i in range(sheet.nrows):
-    if sheet.cell_value(i, 1)==int(excel_date(datetime.datetime.now())):
-        msg=str(sheet.cell_value(i, 0))
+    if sheet.cell_value(i, 1)==int(excel_date(now)):
+        msg="Your tasks for today are: \n"+ str(sheet.cell_value(i, 0))
         print(msg)
         send_email(subject, msg)
     else:
